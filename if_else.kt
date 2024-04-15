d1b841dbdd3fe2d9d4286fe81bc94e1f52e03386
R Hari Narayan <harinarayan2002@gmail.com>
2024-04-16 05:26:58 +0530

    if else conditional statement

---
+++ if_else.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val trafficLightColor= "Green"
+
+    if (trafficLightColor == "Red"){
+        println("Stop")
+    }
+    else if (trafficLightColor == "Yellow"){
+        println("Slow")
+    }
+    else if (trafficLightColor == "Green") {
+        println("Go")
+    }
+    else {
+        println("Invalid color")
+    }
+}
