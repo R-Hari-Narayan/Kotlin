48dac568f5cb9fd344e7131f509f90f8f062c60a
R Hari Narayan <harinarayan2002@gmail.com>
2024-04-16 05:55:10 +0530

    nullability

---
+++ null_in_kotlin.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var favActor: String? = "Pandit Rama Krishna"
+    // ? after String data type makes it a nullable datatype
+    println(favActor)
+    favActor = "Gopala"
+    println(favActor)
+    favActor = null
+    println(favActor)
+}
